6f43c6c051fae0e646015a3f33e3a80b08427cb9
Feraun <artgrib@outlook.com>
2025-04-20 21:34:55 +0300

    Different profiles and categories functional
    Реализованы профили:
    1. Покупатель
    2. Менеджер
    
    Они имеют разные кабинеты
    
    Реализован функционал добавления/удаления категорий через хранимые процедуры

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,16 +8,19 @@ urlpatterns = [
     path('login', views.log_in, name='login'),
 
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('manager_ds/', views.manager_dashboard, name='manager_dashboard'),
 
     path('dashboard/make_order', views.make_order, name='make_ord'),
     path('dashboard/make_order/add/<int:product_id>', views.cart_add, name='cart_add'),
     path('dashboard/make_order/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
 
     path('dashboard/history_of_orders', views.history_of_orders, name='history'),
-
     path('dashboard/support', views.support, name="support"),
 
     path('dashboard/logout', views.logout, name='logout'),
+
+    path('manager_ds/categories', views.mds_categories, name='mds_categories'),
+    path('manager_ds/categories/delete/<int:category_id>', views.mds_category_delete, name='mds_category_delete')
     # path('cart', views.cart_detail, name='cart_detail'),
     # path('cart/add/<int:product_id>', views.cart_add, name='cart_add'),
     # path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),

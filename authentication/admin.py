c9a7e655f65af435bd9d9254094e109fe7ca3fb5
Feraun <artgrib@outlook.com>
2025-04-20 21:32:48 +0300

    Update models and admin-panel
    Добавлены модели:
    1. Категория
    2. Продавец
    
    и они загружены в админку

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,6 +3,8 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Supplier)
 
 class OrderItemInLine(admin.TabularInline):
     model = OrderItem
